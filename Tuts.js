9938c50aa4883f198766bdcd9099ae18a94fe1d0
Srikanth <kvsrikanth.kvs@gmail.com>
2020-07-06 15:29:50 +0530

    commit 1

---
+++ Tuts.js
@@ -0,0 +1,48 @@
+// let name = prompt('enter your name:');
+// let txtType = confirm('The type is string?');
+
+// document.getElementById("hello").innerHTML = `my name is :${name} and the type is :${typeof name} -${txtType}`;
+// Register();
+
+// function Register() {
+//     alert('func declaration!')
+// }
+
+// let funcexp = function() {
+//     alert("function expression");
+// }
+// funcexp();
+
+// let funcarrow = () => alert('arrow function');
+// funcarrow();
+
+let ob1 = new Object();
+let msg = 'message';
+let ob2 = {
+        name: 'srikanth',
+        // multi word property
+        'gender male': true,
+        //computed object
+        [msg]: 'hello there!',
+    }
+    // add property to object
+ob2.height = 5.4;
+alert(ob2.name + ' ' + ob2.height + ' ' + ob2['gender male'] + ' ' + ob2.message);
+//delete
+delete ob2.height;
+
+
+
+
+
+
+
+//-------------------------------------------------------*-------------------------------------------------------
+/*
+keypoints:
+----------
+->function, function expression, arrow function
+->Nullish coalescing operator '??'
+->Computed properties
+->
+*/
