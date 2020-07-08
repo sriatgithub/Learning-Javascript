681a772c5c1ce66f882710421d7a127932e56e41
Srikanth <kvsrikanth.kvs@gmail.com>
2020-07-08 10:25:33 +0530

    changes..

--- Tuts.js
+++ Tuts.js
@@ -26,20 +26,37 @@
 }
 */
 
-let ob1 = new Object();
-let msg = 'message';
-let ob2 = {
-        name: 'srikanth',
-        // multi word property
-        'gender male': true,
-        //computed object
-        [msg]: 'hello there!',
-    }
-    // add property to object
-ob2.height = 5.4;
-alert(ob2.name + ' ' + ob2.height + ' ' + ob2['gender male'] + ' ' + ob2.message);
+// let ob1 = new Object();
+// let msg = 'message';
+// let ob2 = {
+// name: 'srikanth',
+// multi word property
+// 'gender male': true,
+//computed object
+// [msg]: 'hello there!',
+// }
+// add property to object
+// ob2.height = 5.4;
+// alert(ob2.name + ' ' + ob2.height + ' ' + ob2['gender male'] + ' ' + ob2.message);
 //delete
-delete ob2.height;
+// delete ob2.height;
+
+//property value shorthand : if we have variable name and property name same
+// function func1(name) {
+//     return {
+//name:name,
+//same name so use shorthand
+//name,
+// }
+// }
+
+// 'in' operator for finding a property of an object is declared or not
+// let ob1 = {name:'srikanth'};
+// alert(ob1.age); result is undefined bcoz the property is not there in ob1 1
+// let isdeclared = "age" in ob1; 
+
+
+
 
 
 
